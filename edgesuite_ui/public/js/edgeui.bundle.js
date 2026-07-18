e9d1a4ef49cfe44c6df75ba03db354e7c91116b2
olayemigod <olayemigod@gmail.com>
2026-07-18 18:45:31 +0100

    feat(edgeui): activate professional component overrides

--- edgesuite_ui/public/js/edgeui.bundle.js
+++ edgesuite_ui/public/js/edgeui.bundle.js
@@ -1,11 +1,17 @@
 import { baseComponents } from "./edgeui/components";
+import { professionalComponents } from "./edgeui/professional_components";
 import { createEdgeSuiteRuntime, exposeEdgeSuiteRuntime } from "./edgeui/runtime";
 
-export const EDGE_SUITE_UI_VERSION = "0.1.0";
+export const EDGE_SUITE_UI_VERSION = "0.2.0";
+
+const components = Object.freeze({
+  ...baseComponents,
+  ...professionalComponents,
+});
 
 const runtime = createEdgeSuiteRuntime({
   version: EDGE_SUITE_UI_VERSION,
-  components: baseComponents,
+  components,
 });
 
 if (typeof globalThis !== "undefined") {
@@ -13,6 +19,8 @@ if (typeof globalThis !== "undefined") {
 }
 
 export * from "./edgeui/components";
+export * from "./edgeui/icons";
 export * from "./edgeui/product_menu";
+export * from "./edgeui/professional_components";
 export * from "./edgeui/runtime";
 export default runtime;
