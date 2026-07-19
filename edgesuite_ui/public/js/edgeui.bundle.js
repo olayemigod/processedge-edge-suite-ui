ba2e3ebe097ade67eb2ac1d7aa37afb57d576447
olayemigod <olayemigod@gmail.com>
2026-07-19 22:36:31 +0100

    feat(edgeui): expose modal components in shared runtime

--- edgesuite_ui/public/js/edgeui.bundle.js
+++ edgesuite_ui/public/js/edgeui.bundle.js
@@ -1,4 +1,5 @@
 import { baseComponents } from "./edgeui/components";
+import { modalComponents } from "./edgeui/modal_components";
 import { professionalComponents } from "./edgeui/professional_components";
 import { createEdgeSuiteRuntime, exposeEdgeSuiteRuntime } from "./edgeui/runtime";
 
@@ -7,6 +8,7 @@ export const EDGE_SUITE_UI_VERSION = "0.2.0";
 const components = Object.freeze({
   ...baseComponents,
   ...professionalComponents,
+  ...modalComponents,
 });
 
 const runtime = createEdgeSuiteRuntime({
@@ -20,6 +22,7 @@ if (typeof globalThis !== "undefined") {
 
 export * from "./edgeui/components";
 export * from "./edgeui/icons";
+export * from "./edgeui/modal_components";
 export * from "./edgeui/product_menu";
 export * from "./edgeui/professional_components";
 export * from "./edgeui/runtime";
