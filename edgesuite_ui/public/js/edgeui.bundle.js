b4429438f48a37e1b0e2e36ef306afceb4492f5e
olayemigod <olayemigod@gmail.com>
2026-07-20 03:32:38 +0100

    fix(edgeui): apply cross-runtime modal compatibility

--- edgesuite_ui/public/js/edgeui.bundle.js
+++ edgesuite_ui/public/js/edgeui.bundle.js
@@ -2,14 +2,16 @@ import { baseComponents } from "./edgeui/components";
 import { installContextIdentityResolver } from "./edgeui/context_identity";
 import { applyFrappeCompatibility } from "./edgeui/frappe_compat";
 import { modalComponents } from "./edgeui/modal_components";
+import { applyModalCrossRuntimeCompatibility } from "./edgeui/modal_cross_runtime";
 import { suppressNativeNotificationRuntime } from "./edgeui/notification_runtime";
 import { professionalComponents } from "./edgeui/professional_components";
 import { createEdgeSuiteRuntime, exposeEdgeSuiteRuntime } from "./edgeui/runtime";
 import { installSharedShellEnhancements } from "./edgeui/shell_enhancements";
 
-export const EDGE_SUITE_UI_VERSION = "0.3.1";
+export const EDGE_SUITE_UI_VERSION = "0.3.2";
 
 applyFrappeCompatibility(professionalComponents);
+applyModalCrossRuntimeCompatibility(modalComponents);
 
 const components = Object.freeze({
   ...baseComponents,
@@ -34,6 +36,7 @@ export * from "./edgeui/context_identity";
 export * from "./edgeui/frappe_compat";
 export * from "./edgeui/icons";
 export * from "./edgeui/modal_components";
+export * from "./edgeui/modal_cross_runtime";
 export * from "./edgeui/notification_runtime";
 export * from "./edgeui/product_menu";
 export * from "./edgeui/professional_components";
