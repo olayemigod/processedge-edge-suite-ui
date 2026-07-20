17e15cca833516474e660eccd251308317552100
olayemigod <olayemigod@gmail.com>
2026-07-20 01:43:12 +0100

    fix(edgeui): register runtime-neutral modal components

--- edgesuite_ui/public/js/edgeui.bundle.js
+++ edgesuite_ui/public/js/edgeui.bundle.js
@@ -1,11 +1,10 @@
 import { baseComponents } from "./edgeui/components";
 import { applyFrappeCompatibility } from "./edgeui/frappe_compat";
 import { modalComponents } from "./edgeui/modal_components";
-import { modalPortalComponents } from "./edgeui/modal_portal";
 import { professionalComponents } from "./edgeui/professional_components";
 import { createEdgeSuiteRuntime, exposeEdgeSuiteRuntime } from "./edgeui/runtime";
 
-export const EDGE_SUITE_UI_VERSION = "0.2.1";
+export const EDGE_SUITE_UI_VERSION = "0.2.2";
 
 applyFrappeCompatibility(professionalComponents);
 
@@ -13,7 +12,6 @@ const components = Object.freeze({
   ...baseComponents,
   ...professionalComponents,
   ...modalComponents,
-  ...modalPortalComponents,
 });
 
 const runtime = createEdgeSuiteRuntime({
@@ -29,7 +27,6 @@ export * from "./edgeui/components";
 export * from "./edgeui/frappe_compat";
 export * from "./edgeui/icons";
 export * from "./edgeui/modal_components";
-export * from "./edgeui/modal_portal";
 export * from "./edgeui/product_menu";
 export * from "./edgeui/professional_components";
 export * from "./edgeui/runtime";
