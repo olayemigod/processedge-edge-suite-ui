b39c9ab86b3311ce417afdb35041ef6b9f66e783
olayemigod <olayemigod@gmail.com>
2026-07-31 14:53:53 +0100

    style(edgeui): load waffle reliability styles

--- edgesuite_ui/hooks.py
+++ edgesuite_ui/hooks.py
@@ -16,6 +16,7 @@ app_include_css = [
 	"edgeui.bundle.css",
 	"edgeui_compat.bundle.css",
 	"/assets/edgesuite_ui/css/edgeui_product_menu.css",
+	"/assets/edgesuite_ui/css/edgeui_product_menu_reliability.css",
 	"/assets/edgesuite_ui/css/edgeui_professional.css",
 	"/assets/edgesuite_ui/css/edgeui_sidebar_refinement.css",
 	"/assets/edgesuite_ui/css/edgeui_action_contrast.css",
