20847f08376d01d76e6612551b5faa59b758d56b
olayemigod <olayemigod@gmail.com>
2026-08-11 13:15:10 +0100

    chore(edgeui): load theme transition guard

--- edgesuite_ui/hooks.py
+++ edgesuite_ui/hooks.py
@@ -35,6 +35,7 @@ app_include_css = [
 	"/assets/edgesuite_ui/css/edgeui_theme.css",
 	"/assets/edgesuite_ui/css/edgeui_theme_controls.css",
 	"/assets/edgesuite_ui/css/edgeui_theme_dark_compat.css",
+	"/assets/edgesuite_ui/css/edgeui_theme_transition_guard.css",
 ]
 app_include_js = [
 	"/assets/edgesuite_ui/js/edgeui_theme_bootstrap.js",
