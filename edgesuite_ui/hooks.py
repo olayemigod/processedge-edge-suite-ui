63a563f960fe95a5250aebb8e85fa277fbc19af7
olayemigod <olayemigod@gmail.com>
2026-08-19 17:10:17 +0100

    feat(edgeui): load shared report grouping styles

--- edgesuite_ui/hooks.py
+++ edgesuite_ui/hooks.py
@@ -42,6 +42,7 @@ app_include_css = [
 	"/assets/edgesuite_ui/css/edgeui_report_export.css",
 	"/assets/edgesuite_ui/css/edgeui_reporting_presentation.css",
 	"/assets/edgesuite_ui/css/edgeui_report_comparison.css",
+	"/assets/edgesuite_ui/css/edgeui_report_grouping.css",
 ]
 app_include_js = [
 	"/assets/edgesuite_ui/js/edgeui_theme_bootstrap.js",
